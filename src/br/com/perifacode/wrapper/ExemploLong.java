3e2f2563a86e6515e570a36c5ebbc43ce3e69d10
josvieira <josienevieira20@gmail.com>
2020-08-21 21:24:21 -0300

    Primitivo long e wraper Long

--- src/br/com/perifacode/wrapper/ExemploLong.java
+++ src/br/com/perifacode/wrapper/ExemploLong.java
@@ -1,7 +1,19 @@
 package br.com.perifacode.wrapper;
 
+import java.time.LocalTime;
+
 public class ExemploLong {
   public static void main(String[] args) {
+
+    Long resultado = 1L;
+
+    System.out.println("Tempo Inicial " + LocalTime.now());
+    for(int i=0; i<1000; i++){
+      long soma = resultado + i;
+      resultado = soma;
+    }
+
+    System.out.println("Tempo Final " + LocalTime.now());
 
   }
 }
